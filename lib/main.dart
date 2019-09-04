026becb1d98f2b4f2859be3fb7c947e3bcfe3e38
Amanda Mussio <amanda@fliper.app>
2019-09-04 10:10:41 -0300

    form validando

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white
       ),
       home: LoginPage(),
     );
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,17 +2,99 @@ import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
 
+  final _formKey = GlobalKey<FormState>();
+
+  final _tLogin = TextEditingController();
+  final _tSenha = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Carros'),),
+      appBar: AppBar(
+        title: Text('Carros'),
+      ),
       body: _body(),
     );
   }
 
-  Container _body() {
-    return Container(
-      color: Colors.white,
+  Form _body() {
+    return Form(
+          key: _formKey, //gaurda o estado do formulario
+          child: Container(
+          padding: EdgeInsets.all(16),
+          child: ListView(
+            children: <Widget>[
+              textFormField("Login", "Digite o login", controller: _tLogin, validator: _validateLogin),
+              SizedBox(height: 10),
+              textFormField("Senha", "Digite a senha", password: true, controller: _tSenha, validator: _validateSenha),
+              SizedBox(height: 20),
+              _button("Entrar",_onClickLogin),
+            ],
+        ),
+      ),
     );
+  }
+
+  void _onClickLogin() {
+
+    if(_formKey.currentState.validate()) { //executa os validators do form
+      return;
+    }
+
+    String login = _tLogin.text;
+    String senha = _tSenha.text;
+
+    print("Login: $login, Senha: $senha");
+  }
+
+  Container _button(String text, Function onPressed) {
+    return Container(
+        height: 46,
+        child: RaisedButton(
+          color: Colors.blue,
+          child: Text(
+            text,
+            style: TextStyle(color: Colors.white, fontSize: 22,),
+          ),
+          onPressed: onPressed,
+        )
+      );
+  }
+
+  TextFormField textFormField(String label, String hint,
+      {bool password = false, TextEditingController controller, Function validator}) {
+    return TextFormField(
+      controller: controller,
+      validator: validator,
+      obscureText: password,
+      style: TextStyle(fontSize: 25, color: Colors.blue),
+      decoration: InputDecoration(
+          labelText: label,
+          labelStyle: TextStyle(
+            fontSize: 25,
+            color: Colors.grey,
+          ),
+          hintText: hint,
+          hintStyle: TextStyle(
+            fontSize: 16,
+          )),
+    );
+  }
+
+  String _validateLogin(String text) {
+    if(text.isEmpty) {
+      return "Digite o login";
+    }
+    return null;
+  }
+
+  String _validateSenha(String text) {
+    if(text.isEmpty) {
+      return "Digite a senha";
+    }
+    if(text.length < 3) {
+      return "Precisa ter pelo menos 3 caracteres";
+    }
+    return null;
   }
 }
